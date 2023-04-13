f1b9756e4627429638bbec8ebf57df5466900083
gunturajip <gunturaji852@gmail.com>
2023-04-13 18:07:08 +0700

    [folders] delete folders

--- entity/product.go
+++
@@ -1,6 +0,0 @@
-package entity
-
-type Product struct {
-	Id   string
-	Name string
-}
--- repository/product_repository.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import "golang-jwt-auth/entity"
-
-type ProductRepository interface {
-	FindById(id string) *entity.Product
-}
